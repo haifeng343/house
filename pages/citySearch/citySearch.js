35c994f88cdb0a6d676521aeac463e0a16bdfaa4
MrLK <1056622768@.com>
2019-10-12 18:26:59 +0800

    搜索页面修改

--- pages/citySearch/citySearch.js
+++ pages/citySearch/citySearch.js
@@ -346,25 +346,27 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  /**返回上一级 */
+  goBack() {
+    wx.navigateBack({
+      delta: 1
+    });
+  },
+
+  clearInput() {
+    this.setData({ value: '', hasAsked: false})
+  },
+
   onLoad: function (options) {
     let { type } = options;
 
     this.setData({ type })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
     let history = []
     if (this.data.type == 'city') {
@@ -377,37 +379,22 @@ Page({
     })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
   onHide: function () {
 
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
   onUnload: function () {
 
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
   onPullDownRefresh: function () {
 
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
   onReachBottom: function () {
 
   },
 
-  /**
-   * 用户点击右上角分享
-   */
   onShareAppMessage: function () {
 
   }
