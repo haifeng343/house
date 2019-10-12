2a57701372d4141b8efd8cf68a142096dde1fda3
MrLK <1056622768@.com>
2019-10-12 12:04:47 +0800

    样式修改

---
+++ utils/httpAddress.js
@@ -0,0 +1,10 @@
+//房源本地地址 http://192.168.1.53:3001
+//房源测试地址 https://fytest.bangdingding.cn
+export const api_address = 'https://fytest.bangdingding.cn';
+
+//本地地址：http://192.168.1.109:9001
+// 开发环境: http://fddserver.pdd.develop
+// 测试环境: http://fddserver.pdd.test
+// 正式环境: https://piao.bangdingding.cn
+//测试环境https://fangtest.bangdingding.cn
+export const default_address = 'http://192.168.1.80:9001'; 
