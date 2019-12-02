0f2d42b4633a6d78584bd89aa90989a4c0bcd091
陈祉成 <chenzc@fuxinweb.cn>
2019-12-02 11:49:48 +0800

    修改XML2JSON适配后端数据格式

--- utils/xml2json.js
+++ utils/xml2json.js
@@ -3,7 +3,7 @@ class Parser {
 
   error = false;
 
-  xmlString = '';
+  xmlString = "";
 
   json = {};
 
@@ -18,17 +18,17 @@ class Parser {
   }
 
   readDeclaration() {
-    this.cursor = this.xmlString.indexOf('?>');
-    if (this.xmlString.includes('<!DOCTYPE')) {
-      this.cursor = this.xmlString.indexOf(']>') + 2;
+    this.cursor = this.xmlString.indexOf("?>");
+    if (this.xmlString.includes("<!DOCTYPE")) {
+      this.cursor = this.xmlString.indexOf("]>") + 2;
     }
     this.next();
   }
 
   next() {
-    const index = this.xmlString.indexOf('<', this.cursor);
+    const index = this.xmlString.indexOf("<", this.cursor);
     this.cursor =
-      this.xmlString.indexOf('<![CDATA[', index) === 0
+      this.xmlString.indexOf("<![CDATA[", index) === 0
         ? this.cursor
         : index + 1;
   }
@@ -36,7 +36,7 @@ class Parser {
   readNode(sibling) {
     const node = {};
     const nextTest = /^[\s\n]*<(?!\/|!\[CDATA\[)/;
-    const openTagStop = this.xmlString.indexOf('>', this.cursor);
+    const openTagStop = this.xmlString.indexOf(">", this.cursor);
     const openTag = this.readTag(
       this.xmlString.substring(this.cursor, openTagStop)
     );
@@ -48,7 +48,7 @@ class Parser {
     this.cursor = openTagStop + 1;
 
     // not closed
-    if (this.xmlString.indexOf('/>', this.cursor - 2) !== this.cursor - 2) {
+    if (this.xmlString.indexOf("/>", this.cursor - 2) !== this.cursor - 2) {
       // child
       if (
         nextTest.test(this.xmlString.substring(this.cursor, this.cursor + 1024))
@@ -59,7 +59,7 @@ class Parser {
 
       const text = this.readText(openTag.nodeName);
       if (!!text && !/^\s+$/.test(text)) {
-        node['text'] = text;
+        node.content = text;
       }
     }
 
@@ -68,6 +68,15 @@ class Parser {
     ) {
       this.next();
       this.addChildren(sibling || parent, this.readNode(sibling || parent));
+    }
+
+    if (
+      Object.keys(node).length === 0 || // <tag></tag>
+      (Object.keys(node).length === 1 && typeof node.content === "string") // <tag>content text</tag>
+      // <tag foo="bar"></tag> => length => 1  but content => undefined
+      // <tag foo="bar">content text</tag> => length => 2 content => string
+    ) {
+      parent[openTag.nodeName] = node.content || "";
     }
 
     return parent;
@@ -87,23 +96,23 @@ class Parser {
   }
 
   readText(nodeName) {
-    let text = '';
+    let text = "";
     let start = this.cursor;
-    let end = this.xmlString.indexOf('</' + nodeName + '>', start);
+    let end = this.xmlString.indexOf("</" + nodeName + ">", start);
     let cdStart = -1;
     let cdEnd;
     if (end < 0) {
       return text;
     }
     while (
-      (cdStart = this.xmlString.indexOf('<![CDATA[', start)) > -1 &&
+      (cdStart = this.xmlString.indexOf("<![CDATA[", start)) > -1 &&
       cdStart < end
     ) {
       text += transEntities(this.xmlString.substring(start, cdStart));
-      cdEnd = this.xmlString.indexOf(']]>', cdStart);
+      cdEnd = this.xmlString.indexOf("]]>", cdStart);
       text += this.xmlString.substring(cdStart + 9, cdEnd);
       start = cdEnd + 3;
-      end = this.xmlString.indexOf('</' + nodeName + '>', start);
+      end = this.xmlString.indexOf("</" + nodeName + ">", start);
     }
     text += transEntities(this.xmlString.substring(start, end));
     this.cursor = end + nodeName.length + 3;
@@ -128,9 +137,9 @@ class Parser {
 
 const transEntities = str => {
   return str
-    .replace(/&amp;/g, '&')
-    .replace(/&gt;/g, '>')
-    .replace(/&lt;/g, '<')
+    .replace(/&amp;/g, "&")
+    .replace(/&gt;/g, ">")
+    .replace(/&lt;/g, "<")
     .replace(/&quot;/g, '"')
     .replace(/&apos;/g, "'");
 };
@@ -143,4 +152,4 @@ const xml2json = xmlString => {
     throw new Error(result.error);
   }
 };
-export { xml2json }
+export { xml2json };
